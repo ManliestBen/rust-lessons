c6e322b96f967f0814f60a9bbfd13befe64867e0
ManliestBen <benjamintmanley@gmail.com>
2025-04-14 11:59:18 -0500

    add to variables content

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -2,10 +2,17 @@ fn main() {
   let num_tortillas = 5;
   let num_beef_slices = 10;
   let my_name ="Ben";
+
+  let mut counter = 0; // mut is used to make the variable mutable
+  counter = counter + 1;
+  println!("Counter: {}", counter);
   
   let unused_variable = 10;  // unused variable will throw a warning
   let _unused_variable = 10;  // this unused variable will not throw a warning
 
+  #[allow(unused_variables)]  // compiler directive to suppress the warning
+  let another_unused_variable = 10;
+  
   // example of interpolation
   println!("Hello, {}", my_name);
   
@@ -24,4 +31,11 @@ fn main() {
   // another example of concatenation
   println!("I have {num_tortillas} tortillas and {num_beef_slices} beef slices");
 
+  // example of function call
+  print_name(my_name);
+
+}
+
+fn print_name(name: &str) {
+  println!("Hello, {}", name);
 }
