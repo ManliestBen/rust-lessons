5c5fddf37e6b36ff9f9158f7f87a93ce9282235a
ManliestBen <benjamintmanley@gmail.com>
2025-04-23 14:33:44 -0500

    complete control flow content

--- control-flow/src/coding_challenge.rs
+++ control-flow/src/coding_challenge.rs
@@ -27,3 +27,47 @@ Implement two solutions/functions for the problem.
 The first solution should not use recursion.
 The second solution should use recursion.
 */
+
+pub(crate) fn main_test() {
+  println!("{}", color_to_number("green"));
+  println!("{}", color_to_number_match("blue"));
+  println!("{}", factorial(5));
+  println!("{}", factorial_recursion(5));
+}
+
+fn factorial(num: i32) -> i32 {
+  let mut product = 1;
+  for num in 1..num + 1 {
+    product = product * num;
+  }
+  return product
+}
+
+fn factorial_recursion(num: i32) -> i32 {
+  if num == 1 {
+    return 1
+  } else {
+    return num * factorial_recursion(num - 1)
+  }
+}
+
+fn color_to_number(color: &str) -> i32 {
+  if color.to_lowercase() == "red" {
+    return 1
+  } else if color.to_lowercase() == "green" {
+    return 2
+  } else if color.to_lowercase() == "blue" {
+    return 3
+  } else {
+    return 0
+  }
+}
+
+fn color_to_number_match(color: &str) -> i32 {
+  match color {
+    "red" => 1,
+    "green" => 2,
+    "blue" => 3,
+    _ => 0
+  }
+}
--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,1 +1,62 @@
-fn main() {}
+mod coding_challenge;
+
+fn main() {
+
+  // must use values of either true/false with 'if' statements
+  // no concept of 'truthiness/falsiness' like some other languages
+  // no ternary operator in rust
+
+  let age = 42;
+
+  if age >= 21 {
+    println!("Let's get a beer.")
+  } else {
+    println!("Let's get a soda.")
+  }
+
+  even_or_odd(54);
+
+  // match is like switch in other languages
+  let eval = true;
+
+  match eval {
+    // pattern or arm
+    true => {println!("matched true");}
+    false => {println!("matched false");}
+  }
+
+  let drink = match age {
+    21..=i32::MAX => "Beer",
+    18..=20 => "Soda",
+    _ => "Water"
+  };
+  // println!("You can have {drink}.");
+
+  let mut seconds = 10;
+  // loop {
+  //   if seconds > 0 {
+      // println!("{seconds} seconds left!");
+  //     seconds -= 1;
+  //   } else {
+      // println!("Blastoff!");
+  //     break;
+  //   }
+  // }
+
+  while seconds > 0 {
+    println!("{seconds} seconds left");
+    seconds -= 1;
+  }
+
+  coding_challenge::main_test();
+}
+
+
+fn even_or_odd(num: i32) {
+  let result = if num % 2 == 0 {
+    "even"
+  } else {
+    "odd"
+  };
+  println!("The number is {result}")
+}
